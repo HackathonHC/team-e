b7bdf3b58e005b230c35045280c0c01c1336557b
大西直樹 <onishi@cloverlab.jp>
2015-05-16 16:17:05 +0900

    縦画面対応

--- Assets/GameMain.cs
+++ Assets/GameMain.cs
@@ -6,21 +6,21 @@ public class GameMain : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // ボール
-        GameObject ball = (GameObject)Resources.Load("Prefabs/Ball");
+        // GameObject ball = (GameObject)Resources.Load("Prefabs/Ball");
 
-        for (int i = 0; i < 50; i++) {
+        // for (int i = 0; i < 50; i++) {
 
-            //オブジェクトの座標
-            float x = Random.Range(0.0f, 2.0f);
-            float y = Random.Range(0.0f, 2.0f);
-            float z = 0.0f;
+        //     //オブジェクトの座標
+        //     float x = Random.Range(0.0f, 2.0f);
+        //     float y = Random.Range(0.0f, 2.0f);
+        //     float z = 0.0f;
 
-            GameObject go = (GameObject)Instantiate(ball, Vector3.zero, Quaternion.identity);
-            go.transform.parent = transform;
-            go.transform.localScale = new Vector3(50.0f, 50.0f, 0.5f);
-            go.transform.localPosition = new Vector3(x, y, z);
+        //     GameObject go = (GameObject)Instantiate(ball, Vector3.zero, Quaternion.identity);
+        //     go.transform.parent = transform;
+        //     go.transform.localScale = new Vector3(50.0f, 50.0f, 0.5f);
+        //     go.transform.localPosition = new Vector3(x, y, z);
 
-        }
+        // }
 
 	}
 
@@ -28,4 +28,9 @@ public class GameMain : MonoBehaviour {
 	void Update () {
 
 	}
+
+
+    public void ClickStart() {
+        Debug.Log("aaaaaaaaaaaaaaaaaaaaaa");
+    }
 }

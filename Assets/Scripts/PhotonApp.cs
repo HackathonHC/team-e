c0b01df58570158f8b3811c0ddecf3ad29c367f1
Ryo Matsumura <ryooo.321@gmail.com>
2015-05-16 16:14:53 +0900

    アイコン画像を追加

---
+++ Assets/Scripts/PhotonApp.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonApp : Photon.MonoBehaviour
+{
+  void Awake () {
+    //マスターサーバーへ接続
+    PhotonNetwork.ConnectUsingSettings("v0.1");
+  }
+
+  void Update () {
+  }
+
+  //ロビー参加成功時のコールバック
+  void OnJoinedLobby() {
+    //ランダムにルームへ参加
+    RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+    PhotonNetwork.JoinOrCreateRoom("test", roomOptions, TypedLobby.Default);
+  }
+
+  //ルーム参加失敗時のコールバック
+  void OnPhotonRandomJoinFailed() {
+    Debug.Log("ルームへの参加に失敗しました");
+    //名前のないルームを作成
+    PhotonNetwork.CreateRoom("test");
+  }
+
+  //ルーム参加成功時のコールバック
+  void OnJoinedRoom() {
+    Debug.Log("ルームへの参加に成功しました");
+    //playerId = PhotonNetwork.countOfPlayers;
+    Debug.Log(PhotonNetwork.otherPlayers.Length);
+    Debug.Log(PhotonNetwork.isMasterClient);
+    Debug.Log(PhotonNetwork.player.ID);
+
+    // prefab
+    GameObject prefab = (GameObject)Resources.Load("Prefabs/icon");
+
+    // 自分のアイコンを表示
+    GameObject go;
+    go = (GameObject)Instantiate(prefab);
+    go.transform.parent = transform;
+    go.transform.localScale = Vector3.one;
+    go.transform.localPosition = Vector3.zero;
+
+    // 他プレイヤーのアイコンを表示
+    int i = 0;
+    foreach (PhotonPlayer _p in PhotonNetwork.otherPlayers) {
+      go = (GameObject)Instantiate(prefab, Vector3.zero, Quaternion.identity);
+      go.transform.parent = transform;
+      go.transform.localScale = Vector3.one;
+      go.transform.localPosition = new Vector3(-100 + (100 * i), 0, 0);
+    }
+  }
+
+  void OnGUI() {
+    //サーバーとの接続状態をGUIへ表示
+    GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+  }
+
+  void OnClick()
+  {
+    if (PhotonNetwork.connectionStateDetailed != PeerState.Joined)
+    {
+      // only use PhotonNetwork.Instantiate while in a room.
+      return;
+    }
+    PhotonNetwork.Instantiate("Prefabs/PhotonBall", InputToEvent.inputHitPos, Quaternion.identity, 0);
+  }
+}

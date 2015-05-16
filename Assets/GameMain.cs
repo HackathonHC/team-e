3ef99597d4ee7b656242ae00d51fcbadafe3e1bd
大西直樹 <onishi@cloverlab.jp>
2015-05-16 14:58:07 +0900

    調整

--- Assets/GameMain.cs
+++ Assets/GameMain.cs
@@ -1,23 +1,25 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GameMain : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-
-
         // ボール
         GameObject ball = (GameObject)Resources.Load("Prefabs/Ball");
 
-        //オブジェクトの座標
-        float x = Random.Range(0.0f, 2.0f);
-        float y = Random.Range(0.0f, 2.0f);
-        float z = 0.0f;
         for (int i = 0; i < 50; i++) {
-          // プレハブからインスタンスを生成
-          Instantiate(ball, new Vector3(x, y, z), Quaternion.identity);
+
+            //オブジェクトの座標
+            float x = Random.Range(0.0f, 2.0f);
+            float y = Random.Range(0.0f, 2.0f);
+            float z = 0.0f;
+
+            GameObject go = (GameObject)Instantiate(ball, Vector3.zero, Quaternion.identity);
+            go.transform.parent = transform;
+            go.transform.localScale = new Vector3(50.0f, 50.0f, 0.5f);
+            go.transform.localPosition = new Vector3(x, y, z);
+
         }
 
 	}

2b0a33c532f6c34fe53a9ceb1441b8e7b55ea17b
Ryo Matsumura <ryooo.321@gmail.com>
2015-05-17 11:08:00 +0900

    updated

---
+++ Assets/Icon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Icon : MonoBehaviour
+{
+  public string id;
+  void OnClick () {
+    Debug.Log("OnClick");
+    PhotonApp.instance.SetTarget(id);
+  }
+}
